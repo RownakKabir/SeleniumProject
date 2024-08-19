bae28ba1b67a4fb6d79e0971c3cc830f5e51dd33
rownak kabir <rownakkabir007@gmail.com>
2024-08-19 13:55:19 +0600

    Selenium 2nd Assignment

---
+++ src/test/java/selenium3rdassignment/select.java
@@ -0,0 +1,16 @@
+package selenium3rdassignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class select {
+    public static void main(String[] args) {
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://demo.automationtesting.in/Register.html");
+
+        Select select=new Select(driver.findElement(By.cssSelector("select[id='Skills']")));
+        select.selectByIndex(2);
+    }
+}
